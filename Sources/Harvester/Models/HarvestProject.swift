f6c398dd462f9d4ca582d7f3009b924ba694db65
Paul Himes <paul@paulhimes.com>
2026-04-01 22:53:26 -0500

    Make code non-optional.

--- Sources/Harvester/Models/HarvestProject.swift
+++ Sources/Harvester/Models/HarvestProject.swift
@@ -3,13 +3,24 @@ import Foundation
 public struct HarvestProject: Decodable, Sendable {
     public let id: Int
     public let name: String
-    public let code: String?
+    public let code: String
 
     public init(id: Int,
                 name: String,
-                code: String) {
+                code: String = "") {
         self.id = id
         self.name = name
         self.code = code
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case id, name, code
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        code = try container.decodeIfPresent(String.self, forKey: .code) ?? ""
     }
 }
